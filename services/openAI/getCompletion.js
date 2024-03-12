475a47a8f513b7f6a46c4b530f54498b25e26a69
Evan Chime <evanchime@yahoo.co.uk>
2024-03-12 20:43:37 +0000

    Fix generateOrderDetailsObject parameters and generateOrderDetailsObjectFunctinJson schema. Also change context.js to previous version

--- services/openAI/getCompletion.js
+++ services/openAI/getCompletion.js
@@ -97,131 +97,139 @@ const generateOrderDetailsObjectFunctionJson = {
 Input is a json summary of the order.",
     "parameters": {
         "type": "object",
-        "properties": {
-            "reference_id": {
-                "type": "string",
-                "description": "Generate unique identifier for the order or invoice, provided by the business. It is case sensitive and cannot be an empty string and can only contain English letters, numbers, underscores, dashes, or dots, and should not exceed 35 characters."
-            },
-            "total_amount": {
+        "properties":{
+            "orderDetails":{
                 "type": "object",
                 "properties": {
-                    "value": {
-                        "type": "number",
-                        "description": "The total amount of the order or invoice. total_amount.value must be equal to subtotal.value + tax.value + shipping.value - discount.value."
-                    }
-                },
-                "description": "The total amount details for the order"
-            },
-            "items": {
-                "type": "array",
-                "items": {
-                    "type": "object",
-                    "properties": {
-                        "retailer_id": {
-                            "type": "string",
-                            "description": "Generate unique identifier for an item in the order."
+                    "reference_id": {
+                        "type": "string",
+                        "description": "Generate unique identifier for the order or invoice, provided by the business. It is case sensitive and cannot be an empty string and can only contain English letters, numbers, underscores, dashes, or dots, and should not exceed 35 characters."
+                    },
+                    "total_amount": {
+                        "type": "object",
+                        "properties": {
+                            "value": {
+                                "type": "number",
+                                "description": "The total amount of the order or invoice. total_amount.value must be equal to subtotal.value + tax.value + shipping.value - discount.value."
+                            }
                         },
-                        "name": {
-                            "type": "string",
-                            "description": "The item's name to be displayed to the user. Cannot exceed 60 characters"
-                        },
-                        "amount": {
+                        "description": "The total amount details for the order"
+                    },
+                    "items": {
+                        "type": "array",
+                        "items": {
                             "type": "object",
                             "properties": {
-                                "value": {
+                                "retailer_id": {
+                                    "type": "string",
+                                    "description": "Generate unique identifier for an item in the order."
+                                },
+                                "name": {
+                                    "type": "string",
+                                    "description": "The item's name to be displayed to the user. Cannot exceed 60 characters"
+                                },
+                                "amount": {
+                                    "type": "object",
+                                    "properties": {
+                                        "value": {
+                                            "type": "number",
+                                            "description": "The price per item in the order."
+                                        }
+                                    },
+                                    "description": "The amount details per item of the order"
+                                },
+                                "sale_amount": {
+                                    "type": "object",
+                                    "properties": {
+                                        "value": {
+                                            "type": "number",
+                                            "description": "The discounted price per item. This should be less than the original amount. If included, this field is used to calculate the subtotal amount"
+                                        }
+                                    },
+                                    "description": "The sale amount details for the item"
+                                },
+                                "quantity": {
                                     "type": "number",
-                                    "description": "The price per item in the order."
+                                    "description": "The number of this item in the order, this field cannot be decimal has to be integer."
                                 }
                             },
-                            "description": "The amount details per item of the order"
+                            "required": ["retailer_id", "name", "amount", "quantity"]
                         },
-                        "sale_amount": {
-                            "type": "object",
-                            "properties": {
-                                "value": {
-                                    "type": "number",
-                                    "description": "The discounted price per item. This should be less than the original amount. If included, this field is used to calculate the subtotal amount"
-                                }
+                        "description": "The list of items for this order."
+                    },
+                    "subtotal": {
+                        "type": "object",
+                        "properties": {
+                            "value": {
+                                "type": "number",
+                                "description": "The value must be equal to sum of (amount per item multiplied by quantity per item)"
+                            }
+                        },
+                        "description": "The subtotal details for the order"
+                    },
+                    "tax": {
+                        "type": "object",
+                        "properties": {
+                            "value": {
+                                "type": "number",
+                                "description": "The tax amount for the order."
                             },
-                            "description": "The sale amount details for the item"
+                            "description": {
+                                "type": "string",
+                                "description": "The description of the tax. It cannot exceed 60 characters."
+                            },
                         },
-                        "quantity": {
-                            "type": "number",
-                            "description": "The number of this item in the order, this field cannot be decimal has to be integer."
-                        }
+                        "required": ["value"],
+                        "description": "The tax details for the order"
                     },
-                    "required": ["retailer_id", "name", "amount", "quantity"]
-                },
-                "description": "The list of items for this order."
-            },
-            "subtotal": {
-                "type": "object",
-                "properties": {
-                    "value": {
-                        "type": "number",
-                        "description": "The value must be equal to sum of (amount per item multiplied by quantity per item)"
-                    }
-                },
-                "description": "The subtotal details for the order"
-            },
-            "tax": {
-                "type": "object",
-                "properties": {
-                    "value": {
-                        "type": "number",
-                        "description": "The tax amount for the order."
+                    "shipping": {
+                        "type": "object",
+                        "properties": {
+                            "value": {
+                                "type": "number",
+                                "description": "The shipping cost for the order."
+                            },
+                            "description": {
+                                "type": "string",
+                                "description": "The description of the shipping cost. It cannot exceed 60 characters."
+                            },
+                        },
+                        "required": ["value"],
+                        "description": "The shipping details for the order"
                     },
-                    "description": {
-                        "type": "string",
-                        "description": "The description of the tax. It cannot exceed 60 characters."
-                    },
-                },
-                "required": ["value"],
-                "description": "The tax details for the order"
-            },
-            "shipping": {
-                "type": "object",
-                "properties": {
-                    "value": {
-                        "type": "number",
-                        "description": "The shipping cost for the order."
-                    },
-                    "description": {
-                        "type": "string",
-                        "description": "The description of the shipping cost. It cannot exceed 60 characters."
+                    "discount": {
+                        "type": "object",
+                        "properties": {
+                            "value": {
+                                "type": "number",
+                                "description": "The discount amount for the order."
+                            },
+                            "description": {
+                                "type": "string",
+                                "description": "The description of the discount. It cannot exceed 60 characters."
+                            },
+                            "discount_program_name": {
+                                "type": "string",
+                                "description": "Text used for defining incentivised orders. If order is incentivised, define this information. Max character limit is 60 characters"
+                            },
+                        },
+                        "required": ["value"],
+                        "description": "The discount details for the order"
                     },
                 },
-                "required": ["value"],
-                "description": "The shipping details for the order"
-            },
-            "discount": {
-                "type": "object",
-                "properties": {
-                    "value": {
-                        "type": "number",
-                        "description": "The discount amount for the order."
-                    },
-                    "description": {
-                        "type": "string",
-                        "description": "The description of the discount. It cannot exceed 60 characters."
-                    },
-                    "discount_program_name": {
-                        "type": "string",
-                        "description": "Text used for defining incentivised orders. If order is incentivised, define this information. Max character limit is 60 characters"
-                    },
-                },
-                "required": ["value"],
-                "description": "The discount details for the order"
+                "required": ["reference_id", "total_amount", "items", "subtotal"]
             },
         },
-        "required": ["reference_id", "total_amount", "items", "subtotal"],
-    },
+        "description": "The order details to be processed.",
+        "required": ["orderDetails"]
+    }
 }
+        
 
 const generateOrderDetailsObject = async ({
     reference_id,
     total_amount: { value },
-    items: items,
+    items,
     subtotal: { value: subtotalValue },
     tax: { value: taxValue, description: taxDescription },
     shipping: { value: shippingValue, description: shippingDescription },
