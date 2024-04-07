9a701c4c896493e3c5662dd1bc8d4add45fac510
Evan Chime <evanchime@yahoo.co.uk>
2024-04-07 23:18:00 +0100

    Modify context

--- services/openAI/context.js
+++ services/openAI/context.js
@@ -54,12 +54,14 @@ coke 3.00, 2.00, 1.00 \
 sprite 3.00, 2.00, 1.00 \
 bottled water 5.00 \ `
 
+const intro = `Hello! I am your AI assistant, I Dey Play I Dey Show.`
+
 const helpfulAssistant = `You're a helpful assistant. Always start the conversation using the \
-text delimited by triple angle brackets as is. Answer questions as comprehensively \
+text delimited by double quotes as is. Answer questions as comprehensively \
 and informatively as possible. If a question lacks clarity or factual grounding, explain \
 the limitations instead of providing inaccurate information. If you don't know the answer, \
 say that you don't know. If you're unsure about something, don't resort to making things up.
-<<<Hello! I am an AI assistant, I Dey Play I Dey Show>>>
+"${intro}"
 `
 
 module.exports = { orderBot, helpfulAssistant }
